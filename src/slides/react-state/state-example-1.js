84d7cb56128011e24b920976779576c8b162f34d
Purvi Kanal <purvikanal92@gmail.com>
2018-06-26 15:57:14 -0400

    🚒 fixed the react state examples to use the constructor (#20)

--- src/slides/react-state/state-example-1.js
+++ src/slides/react-state/state-example-1.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export class StatefulComponent extends React.Component {
-
   // in classes, you can assign properties to the object like this, it sort of looks like
   // object notation, and it takes a little while to get used to.
   // Think of it like this - when you make an Instance of StatefulComponent, it will have .state on it as a property
   // inside of the definition of of the class, you can reference state by using the this keyword! ie, this.state
-  state = {
-    name: 'Kamala Khan',
-    age: 17
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Kamala Khan",
+      age: 17
+    };
+  }
 
   render() {
-    return <div>
-      Hello {this.state.name},
-      Can't believe you're {this.state.age} years old!
-    </div>
+    return (
+      <div>
+        Hello {this.state.name}, Can't believe you're {this.state.age} years
+        old!
+      </div>
+    );
   }
 }
--- src/slides/react-state/state-example-4.js
+++ src/slides/react-state/state-example-4.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 
 export class StatefulComponent extends React.Component {
-  state = {
-    name: 'Carol Danvers',
-    age: 17
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Carol Danvers",
+      age: 17
+    };
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     // updating the value of name this way triggers a re-render, meaning the hello {this.state.name} will correctly
     //display Kamala Khan!
-    this.setState({name: 'Kamala Khan'})
+    this.setState({ name: "Kamala Khan" });
   }
 
   render() {
-    return <div>
-      Hello {this.state.name},
-      Can't believe you're {this.state.age} years old!
-    </div>
+    return (
+      <div>
+        Hello {this.state.name}, Can't believe you're {this.state.age} years
+        old!
+      </div>
+    );
   }
 }
