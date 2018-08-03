ed5eb09b53241886ec520763b11e75c2cfd1f474
Anya Craig <AnyaCraig@users.noreply.github.com>
2018-08-03 16:17:58 -0400

    Modularized slides into discrete decks.
    Added routing for individual slide decks.

--- src/presentation.js
+++ src/presentation.js
@@ -3,50 +3,15 @@ import React from 'react';
 import { Deck } from 'spectacle';
 import createTheme from 'spectacle/lib/themes/default';
 import { colours, fontFamilies } from './theme';
-
-import {
-  FAnatomyParameterSlideSet,
-  FAnatomyReturnSlideSet,
-  FPFilterSlideSet,
-  FPMapSlideSet,
-  immutabilitySlideSet,
-  OrderOfOperationsSlideSet,
-  FAnatomyPureSlideSet,
-  ArrowSlideSet,
-  PromisesSlideSet,
-  FCallbackSlideset,
-  reactComponentsIntroSlideSet,
-  functionComponentSlideSet,
-  classComponentSlideSet,
-  reactPropsSlideSet,
-  lifecycleSlideSet,
-  reactStateSlideSet,
-  reduxIntroSet,
-  reduxThunksIntroSet,
-} from './slides';
-import { ReduceSlideSet } from './slides/functional-programming-reduce';
-
-const slideList = [
-  ...FAnatomyReturnSlideSet,
-  ...FAnatomyParameterSlideSet,
-  ...FAnatomyPureSlideSet,
-  ...FCallbackSlideset,
-  ...ArrowSlideSet,
-  ...FPMapSlideSet,
-  ...FPFilterSlideSet,
-  ...ReduceSlideSet,
-  ...OrderOfOperationsSlideSet,
-  ...immutabilitySlideSet,
-  ...PromisesSlideSet,
-  ...reactComponentsIntroSlideSet,
-  ...functionComponentSlideSet,
-  ...classComponentSlideSet,
-  ...reactPropsSlideSet,
-  ...lifecycleSlideSet,
-  ...reactStateSlideSet,
-  ...reduxIntroSet,
-  ...reduxThunksIntroSet,
-];
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {       
+  functionsSlideList,
+  functionalProgrammingSlideList,
+  interpreterSlideList,
+  promisesSlideList,
+  reactSlideList, 
+  reduxSlideList 
+} from './slideDecks'; // get the slide decks
 
 // Require CSS
 require('normalize.css');
@@ -54,14 +19,94 @@ require('./style.css');
 
 const theme = createTheme(colours, fontFamilies);
 
+const contentModules = [
+  {
+    path: "functions",
+    deck: functionsSlideList,
+    title: "Functions",
+  },
+  {
+    path: "functional-programming",
+    deck: functionalProgrammingSlideList,
+    title: "Functional Programming",
+  },
+  {
+    path: "interpreter",
+    deck: interpreterSlideList,
+    title: "The Interpreter",
+  },
+  {
+    path: "promises",
+    deck: promisesSlideList,
+    title: "Promises",
+  },
+  {
+    path: "react",
+    deck: reactSlideList,
+    title: "React",
+  },
+  {
+    path: "redux",
+    deck: reduxSlideList,
+    title: "Redux",
+  },
+];
+
 export default class Presentation extends React.Component {
-  render() {
+
+  renderSlides = (slideList) => {
+    return slideList.map((WorkshopSlide, i) => (
+      <WorkshopSlide key={'slide' + i} />
+    ));
+  }
+
+  renderSlideDeck = (routeProps, slideList) => { 
     return (
       <Deck transition={[]} transitionDuration={0} progress="bar" theme={theme}>
-        {slideList.map((WorkshopSlide, i) => (
-          <WorkshopSlide key={'slide' + i} />
-        ))}
+        { this.renderSlides(slideList) }
       </Deck>
+    );
+  }
+
+  createRoute = (module, key) => {
+    return (
+      <Route path={`/${module.path}/`} key={key} render={(props) => this.renderSlideDeck(props, module.deck)} />
+    );
+  }
+
+  createRouteList = (modules) => {
+    return modules.map((module, i) => this.createRoute(module, i));
+  }
+
+  createLinkList = (modules) => {
+    return (
+      <ul className="slide-deck-link-list">
+        {
+          modules.map((module, i) => {
+            return (
+              <li key={i} className="link-section">
+                  <Link className="slide-deck-link" to={`/${module.path}/`}>{module.title}</Link>
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="presentation-container">
+          <div className="routes-list">
+            {this.createRouteList(contentModules)}
+          </div>
+          <div className="links-list">
+            <h2 className="links-heading">Choose a slide deck by topic:</h2>
+            {this.createLinkList(contentModules)}
+          </div>
+        </div>
+      </Router>
     );
   }
 }
