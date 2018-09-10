eb940ab3a737e9408ed930b0b56a869ec44ef016
shaun lloyd <shaunlloydmedia@gmail.com>
2018-09-10 11:50:06 -0400

    Fix break in react slides

--- src/slides/react-component-lifecycle/lifecycle.js
+++ src/slides/react-component-lifecycle/lifecycle.js
@@ -1,15 +1,14 @@
-import React from "react";
-import { Slide, Heading, Text, List, ListItem, Link } from "spectacle";
-import { ListSlideMaker } from "../../tools";
+import React from 'react';
+import { Slide, Heading, Text, List, ListItem, Link } from 'spectacle';
+import { ListSlideMaker } from '../../tools';
 
 class OverviewSlide extends React.Component {
   render() {
     return (
-      <Slide bgColor="primary" transition={["slide"]} {...this.props}>
+      <Slide bgColor="primary" transition={['slide']} {...this.props}>
         <Heading textSize="75">The Component Lifecycle Events</Heading>
         <Text textSize="32" textColor="white">
-          Events that fire throughout the lifecycle of a component. There are
-          events for:
+          Events that fire throughout the lifecycle of a component. There are events for:
         </Text>
         <List>
           <ListItem textSize="32" margin="0 0 1.5rem 0">
@@ -27,71 +26,60 @@ class OverviewSlide extends React.Component {
   }
 }
 
-const MountSlide = ListSlideMaker(`Mounting Events`);
+const MountSlide = ListSlideMaker(`Mounting Events`, []);
 
 const ConstructorSlide = ListSlideMaker(`constructor (props)`, [
   `Called to construct an instance of the component class`,
-  `Used to initialize state and bind methods`
+  `Used to initialize state and bind methods`,
 ]);
 
-const DerivedStateSlide = ListSlideMaker(
-  `getDerivedStateFromProps (props, state)`,
-  [
-    `Called just before the render method. Both on initial mount and updates`,
-    `Used to help compute state based on a prop change`,
-    `Returns an object to update the state or null to update nothing`,
-    `Should be used sparingly`
-  ]
-);
+const DerivedStateSlide = ListSlideMaker(`getDerivedStateFromProps (props, state)`, [
+  `Called just before the render method. Both on initial mount and updates`,
+  `Used to help compute state based on a prop change`,
+  `Returns an object to update the state or null to update nothing`,
+  `Should be used sparingly`,
+]);
 
 const DidMountSlide = ListSlideMaker(`componentDidMount ()`, [
   `Called once the component is successfully mounted (rendered into the DOM)`,
-  `A great place to make fetch calls`
+  `A great place to make fetch calls`,
 ]);
 
-const UpdateSlide = ListSlideMaker(`Updating Events`);
+const UpdateSlide = ListSlideMaker(`Updating Events`, []);
 
-const ShouldUpdateSlide = ListSlideMaker(
-  `shouldComponentUpdate (nextProps, nextState)`,
-  [
-    `Called after getDerivedStateFromProps`,
-    `Used to return a boolean to determine whether the component should rerender`,
-    `Useful but should not be needed often`
-  ]
-);
+const ShouldUpdateSlide = ListSlideMaker(`shouldComponentUpdate (nextProps, nextState)`, [
+  `Called after getDerivedStateFromProps`,
+  `Used to return a boolean to determine whether the component should rerender`,
+  `Useful but should not be needed often`,
+]);
 
-const DidUpdateSlide = ListSlideMaker(
-  `componentDidUpdate (previousProps, PreviousState)`,
-  [
-    `Called right after the component is updated in the DOM`,
-    `Good place to make HTTP requests as long as you compare your previous state and props their current values`,
-    `Can call this.setState as long is it wrapped in a condition to avoid an infinite loop`
-  ]
-);
+const DidUpdateSlide = ListSlideMaker(`componentDidUpdate (previousProps, PreviousState)`, [
+  `Called right after the component is updated in the DOM`,
+  `Good place to make HTTP requests as long as you compare your previous state and props their current values`,
+  `Can call this.setState as long is it wrapped in a condition to avoid an infinite loop`,
+]);
 
-const UnmountSlide = ListSlideMaker(`Unmounting Events`);
+const UnmountSlide = ListSlideMaker(`Unmounting Events`, []);
 
 const WillUnmountSlide = ListSlideMaker(`componentWillUnmount ()`, [
   `Called before the component is unmounted and destroyed`,
-  `Used for clean up like removing timers, cancelling network requests, etc`
+  `Used for clean up like removing timers, cancelling network requests, etc`,
 ]);
 
 const DeprecatedSlide = ListSlideMaker(`Deprecated Events`, [
   `componentWillMount`,
   `componentWillUpdate`,
-  `componentWillRecieveProps`
+  `componentWillRecieveProps`,
 ]);
 
 class ResourcesSlide extends React.Component {
   render() {
     return (
-      <Slide bgColor="primary" transition={["slide"]} {...this.props}>
+      <Slide bgColor="primary" transition={['slide']} {...this.props}>
         <Heading textSize="75">Resources</Heading>
         <List>
           <ListItem textSize="32" margin="0 0 1.5rem 0">
-            <Link href="https://reactjs.org/docs/react-component.html">
-              Documentation for all lifecycle methods
-            </Link>
+            <Link href="https://reactjs.org/docs/react-component.html">Documentation for all lifecycle methods</Link>
           </ListItem>
           <ListItem textSize="32" margin="0 0 1.5rem 0">
             <Link href="http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/">
@@ -116,5 +104,5 @@ export const lifecycleSlideSet = [
   UnmountSlide,
   WillUnmountSlide,
   DeprecatedSlide,
-  ResourcesSlide
+  ResourcesSlide,
 ];
