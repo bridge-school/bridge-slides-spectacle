2278808a7aba2e4b34c2df79ed64c942fe3df369
Purvi Kanal <purvikanal92@gmail.com>
2019-01-08 15:53:00 -0500

    🔗 update links for cohort 6

--- src/links/index.js
+++ src/links/index.js
@@ -1,57 +1,44 @@
 export const PARAMETERS = {
-  invite: "https://repl.it/classroom/invite/XvipfNO",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492285/0339cc5f2176593cbe1e6a6b6cf40cb2"
+  invite: 'https://repl.it/classroom/invite/XvipfNO',
+  exercise1: 'https://repl.it/student_embed/assignment/1492285/0339cc5f2176593cbe1e6a6b6cf40cb2',
 };
 
 export const RETURN = {
-  invite: "https://repl.it/classroom/invite/Xvb4pIt",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492272/e1e304a8fbe1dad1eefaddbe897bdae6",
-  exercise2:
-    "https://repl.it/student_embed/assignment/1492271/4257dd0bdc509b2cc123249c1ff02526"
+  invite: 'https://repl.it/classroom/invite/Xvb4pIt',
+  exercise1: 'https://repl.it/student_embed/assignment/1492272/e1e304a8fbe1dad1eefaddbe897bdae6',
+  exercise2: 'https://repl.it/student_embed/assignment/1492271/4257dd0bdc509b2cc123249c1ff02526',
 };
 
 export const ARROW_FUNCTIONS = {
-  invite: "https://repl.it/classroom/invite/XvedktC",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492279/4905683f6a600f95f4b74d3003cec53a"
+  invite: 'https://repl.it/classroom/invite/XvedktC',
+  exercise1: 'https://repl.it/student_embed/assignment/1492279/4905683f6a600f95f4b74d3003cec53a',
 };
 
 export const CALLBACKS = {
-  invite: "https://repl.it/classroom/invite/XvhmgdL",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492283/5586c54efd8e2124d7a123f30e8b20f8"
+  invite: 'https://repl.it/classroom/invite/4Zjq5QB',
+  exercise1: 'https://repl.it/student_embed/assignment/2411244/c07f8cfc1c44fffff93dada539f1a94e',
 };
 
 export const PURE_FUNCTIONS = {
-  invite: "https://repl.it/classroom/invite/XvdamSz",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492278/37a21c5b28239a43b266961c089ed44d"
+  invite: 'https://repl.it/classroom/invite/4Zjq5QB',
+  exercise1: 'https://repl.it/student_embed/assignment/2410962/0daba390c33aaf5bffc4477b5232af85',
 };
 
 export const FILTER = {
-  invite: "https://repl.it/classroom/invite/XvJM11a",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1490944/600947e6a9cee06b8ed387e0b7cecd0c",
-  exercise2:
-    "https://repl.it/student_embed/assignment/1490943/7cf916312a71aab62f56a16b78903406"
+  invite: 'https://repl.it/classroom/invite/4Zjq5QB',
+  exercise1: 'https://repl.it/student_embed/assignment/2410967/d5b7a1013743dbc7155c704b67907cd3',
+  exercise2: 'https://repl.it/student_embed/assignment/1490943/7cf916312a71aab62f56a16b78903406',
 };
 
 export const MAP = {
-  invite: "https://repl.it/classroom/invite/XvIJ2r7",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1490942/91e0113f0ac30c223dfa0f58280ece73",
-  exercise2:
-    "https://repl.it/student_embed/assignment/1490941/0cd4dd9dc924589b1814a3d307e654d8"
+  invite: 'https://repl.it/classroom/invite/4Zjq5QB',
+  exercise1: 'https://repl.it/student_embed/assignment/2410966/4267917d63e71d9fc13e3d24fe0bac68',
+  exercise2: 'https://repl.it/student_embed/assignment/1490941/0cd4dd9dc924589b1814a3d307e654d8',
 };
 
 export const REDUCE = {
-  invite: "https://repl.it/classroom/invite/Xvc7niw",
-  exercise1:
-    "https://repl.it/student_embed/assignment/1492273/492ac8a84f624191efd3c043e955ff92",
-  exercise2:
-    "https://repl.it/student_embed/assignment/1492277/6369db51be4ea2fd4243a68a1a51a1dd",
-  exercise3:
-    "https://repl.it/student_embed/assignment/1492276/ee34964c023ce10ef6d37129343a0e7d"
+  invite: 'https://repl.it/classroom/invite/4Zjq5QB',
+  exercise1: 'https://repl.it/student_embed/assignment/2410969/2ce8cc165054e840c0e2d8749c03f234',
+  exercise2: 'https://repl.it/student_embed/assignment/1492277/6369db51be4ea2fd4243a68a1a51a1dd',
+  exercise3: 'https://repl.it/student_embed/assignment/1492276/ee34964c023ce10ef6d37129343a0e7d',
 };
--- src/slides/function-anatomy-return/function-anatomy-return.js
+++ src/slides/function-anatomy-return/function-anatomy-return.js
@@ -1,27 +1,14 @@
-import * as returnCode from "!raw-loader!./function-anatomy-return.example";
-import {
-  BasicCodeSlideMaker,
-  ListSlideMaker,
-  ReplClassSignup,
-  ReplFrameSlide
-} from "../../tools";
-import { RETURN } from "../../links";
+import * as returnCode from '!raw-loader!./function-anatomy-return.example';
+import { BasicCodeSlideMaker, ListSlideMaker, ReplClassSignup } from '../../tools';
+import { RETURN } from '../../links';
 
-const Slide1 = ListSlideMaker("Returns", [
-  "A return statement is something you should have in almost all of your functions",
+const Slide1 = ListSlideMaker('Returns', [
+  'A return statement is something you should have in almost all of your functions',
   "Without a return, a function resolves to 'undefined'",
-  "You can be very creative about what is returned - for example, you can return another function",
-  "The result of a function return can be used immediately after that function has finished its business"
+  'You can be very creative about what is returned - for example, you can return another function',
+  'The result of a function return can be used immediately after that function has finished its business',
 ]);
 
-const Slide2 = ReplClassSignup("Returns", RETURN.invite);
-const Slide3 = ReplFrameSlide(RETURN.exercise1);
-const Slide4 = ReplFrameSlide(RETURN.exercise2);
+const Slide2 = ReplClassSignup('Returns', RETURN.invite);
 
-export const FAnatomyReturnSlideSet = [
-  Slide1,
-  BasicCodeSlideMaker(returnCode),
-  Slide2,
-  Slide3,
-  Slide4
-];
+export const FAnatomyReturnSlideSet = [Slide1, BasicCodeSlideMaker(returnCode), Slide2];
