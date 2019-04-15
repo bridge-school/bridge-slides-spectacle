bea4b16c2291de32fc663aed7a6d1dadef44c0b2
Leonardo Rota-Rossi <leorossi@gmail.com>
2019-04-15 09:44:42 -0400

    Fixes attribution order on slide #21 (#55)

--- src/slides/immutability/immutable-array-code.example.js
+++ src/slides/immutability/immutable-array-code.example.js
@@ -1,7 +1,7 @@
 const myFriendsLastYear = ['Elliot', 'Mustafa', 'Rehana', 'Sarah'];
 
-myFriendsLastYear.push('Yihua');
 const myFriendsThisYear = myFriendsLastYear;
+myFriendsThisYear.push('Yihua');
 
 console.log(myFriendsThisYear); // correctly shows all 5 of my current friends, including Yihua
 console.log(myFriendsLastYear); // unfortunately ALSO includes Yihua, we mutated the original array using push
